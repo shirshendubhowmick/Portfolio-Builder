96254396156d5289a157756d9a41350661300e29
Shirshendu Bhowmick <shirshendu_0@yahoo.co.in>
2018-08-25 22:49:59 +0530

    Portfolio Component added and WIP

---
+++ src/Portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Badge from '../Badge/Badge'
+
+class portfolio extends Component {
+    render() {
+        return (
+            <div className="portfolio">
+                <Badge className="badge_portfolio">MANAGE PORTFOLIO</Badge>
+            </div>
+        );
+    }
+};
+
+export default portfolio;
