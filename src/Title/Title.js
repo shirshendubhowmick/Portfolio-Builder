eb4ca668eef5ab91371d5aa9e65b27e9eab07560
Shirshendu Bhowmick <shirshendu_0@yahoo.co.in>
2018-09-12 22:47:50 +0530

    Final Commit

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Title.css'
 
 const title = () => {
-    return <div className={classes.title}>smallcase Portfolio Builder</div>
+    return <div className={classes.title}>my Portfolio Builder</div>
 }
 
 export default title;
